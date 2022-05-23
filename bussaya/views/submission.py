577e7a30349b2ea7e0d512878609fb432a692a04
Chanawat Thuasuphap <earthza991@hotmail.com>
2022-05-23 16:51:37 +0700

    Create submission view page

--- bussaya/views/submission.py
+++ bussaya/views/submission.py
@@ -9,20 +9,18 @@ import datetime
 module = Blueprint("submission", __name__, url_prefix="/")
 
 
-@module.route("/<class_id>/submission/add", methods=["GET", "POST"])
+@module.route("/<class_id>/submissions/add", methods=["GET", "POST"])
 @login_required
 def add(class_id):
     form = forms.submissions.SubmissionForm()
     class_ = models.Class.objects.get(id=class_id)
     if not form.validate_on_submit():
-        print("TEST")
         return render_template(
             "/submissions/add.html",
             form=form,
             class_=class_,
         )
 
-    print(form.validate_on_submit())
     submissions = models.Submission()
     form.populate_obj(submissions)
     submissions.class_ = class_
@@ -31,4 +29,17 @@ def add(class_id):
 
     return redirect(
         url_for("admin.classes.view", class_id=class_id, submissions=submissions)
+    )
+
+
+@module.route(
+    "/admin/classes/<class_id>/submissions/<submission_id>", methods=["GET", "POST"]
+)
+@login_required
+def view(submission_id, class_id):
+    form = forms.submissions.SubmissionForm()
+    submission = models.Submission.objects.get(id=submission_id)
+    class_ = models.Class.objects.get(id=class_id)
+    return render_template(
+        "/submissions/view.html", submission=submission, class_=class_
     )
