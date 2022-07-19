c98e622feaf60aa6e51171bcaf5b6c47e49c2430
Thanathip Limna <boatkrap@gmail.com>
2022-07-19 22:45:01 +0700

    fix google data

--- bussaya/oauth2.py
+++ bussaya/oauth2.py
@@ -1,11 +1,11 @@
 from flask import g, config, session, redirect, url_for
 from flask_login import current_user, login_user
 from authlib.integrations.flask_client import OAuth
+import requests
+import datetime
 
 from . import models
 import mongoengine as me
-
-import datetime
 
 
 def fetch_token(name):
@@ -80,8 +80,10 @@ def create_user_facebook(user_info):
 
 def get_user_info(remote, token):
     if remote.name == "google":
-        resp = remote.get("userinfo")
-        return resp.json()
+        # resp = remote.get("userinfo")
+        # return resp.json()
+        print(token)
+        return token["userinfo"]
     elif remote.name == "facebook":
         USERINFO_FIELDS = [
             "id",
@@ -197,55 +199,11 @@ def handle_authorize(remote, token, user_info):
     return redirect(url_for("dashboard.index"))
 
 
-# def create_flask_blueprint(backend, oauth, handle_authorize):
-#     from flask import Blueprint, request, url_for, current_app, session
-#     from authlib.flask.client import RemoteApp
-#     from authlib.common.security import generate_token
-#     from loginpass._core import register_to
-
-#     remote = register_to(backend, oauth, RemoteApp)
-#     nonce_key = '_{}:nonce'.format(backend.OAUTH_NAME)
-#     bp = Blueprint('loginpass_' + backend.OAUTH_NAME, __name__)
-
-#     @bp.route('/auth')
-#     def auth():
-#         id_token = request.args.get('id_token')
-#         if request.args.get('code'):
-#             token = remote.authorize_access_token(verify=False)
-#             if id_token:
-#                 token['id_token'] = id_token
-#         elif id_token:
-#             token = {'id_token': id_token}
-#         elif request.args.get('oauth_verifier'):
-#             # OAuth 1
-#             token = remote.authorize_access_token(verify=False)
-#         else:
-#             # handle failed
-#             return handle_authorize(remote, None, None)
-#         if 'id_token' in token:
-#             nonce = session[nonce_key]
-#             user_info = remote.parse_openid(token, nonce)
-#         else:
-#             user_info = remote.profile(token=token)
-#         return handle_authorize(remote, token, user_info)
-
-#     @bp.route('/login')
-#     def login():
-#         redirect_uri = url_for('.auth', _external=True)
-#         conf_key = '{}_AUTHORIZE_PARAMS'.format(backend.OAUTH_NAME.upper())
-#         params = current_app.config.get(conf_key, {})
-#         if 'oidc' in backend.OAUTH_TYPE:
-#             nonce = generate_token(20)
-#             session[nonce_key] = nonce
-#             params['nonce'] = nonce
-#         return remote.authorize_redirect(redirect_uri, **params)
-
-#     return bp
-
-
 def init_oauth(app):
     oauth2_client.init_app(app, fetch_token=fetch_token, update_token=update_token)
 
-    # oauth2_client.register('principal')
     oauth2_client.register("engpsu")
-    oauth2_client.register("google")
+    oauth2_client.register(
+        "google",
+        server_metadata_url=app.config.get("GOOGLE_METADATA_URL"),
+    )
