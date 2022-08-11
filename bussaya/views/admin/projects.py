71cd44c27540a9cfcc4194cec80e882233bd60cc
Thanathip Limna <boatkrap@gmail.com>
2022-08-11 18:53:59 +0700

    add admin project edit module

--- bussaya/views/admin/projects.py
+++ bussaya/views/admin/projects.py
@@ -17,6 +17,38 @@ from bussaya import acl
 module = Blueprint("projects", __name__, url_prefix="/projects")
 
 
+@module.route("/<project_id>/edit", methods=["GET", "POST"])
+@acl.roles_required("admin")
+def edit(project_id):
+    project = models.Project.objects.get(id=project_id)
+
+    if not project:
+        return redirect("dashboard.index")
+
+    form = forms.projects.ProjectForm(obj=project)
+
+    lecturers = models.User.objects(roles="lecturer").order_by("first_name")
+    form.advisor.queryset = lecturers
+    form.committees.queryset = lecturers
+
+    # classes = models.Class.objects(student_ids=current_user.username).order_by("-id")
+
+    students = models.User.objects(username__regex="^[0-9]*$").order_by("-username")
+    form.students.queryset = students
+
+    if not form.validate_on_submit():
+        return render_template("/projects/create-edit.html", form=form)
+
+    form.populate_obj(project)
+    if project.advisor in project.committees:
+        project.committees.remove(project.advisor)
+    # project.creator = current_user._get_current_object()
+
+    project.save()
+
+    return redirect(url_for("dashboard.index"))
+
+
 @module.route("/<project_id>/delete")
 @acl.roles_required("admin")
 def delete(project_id):
