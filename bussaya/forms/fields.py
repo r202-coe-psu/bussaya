31ae4e6765103bbb4efa10aa6d5c3bc23027778d
Thanathip Limna <boatkrap@gmail.com>
2022-08-11 23:27:31 +0700

    can add user with username and email

--- bussaya/forms/fields.py
+++ bussaya/forms/fields.py
@@ -5,24 +5,26 @@ from wtforms.fields import Field
 class TagListField(Field):
     widget = widgets.TextInput()
 
-    def __init__(self, label='', validators=None, remove_duplicates=True, **kwargs):
+    def __init__(self, label="", validators=None, remove_duplicates=True, **kwargs):
         super().__init__(label, validators, **kwargs)
         self.remove_duplicates = remove_duplicates
 
     def process_formdata(self, valuelist):
+        self.data = []
         if valuelist:
-            self.data = [x.strip() for x in valuelist[0].split(',')]
-        else:
-            self.data = []
-
-        if self.remove_duplicates:
-            self.data = list(self._remove_duplicates(self.data))
+            for l in valuelist[0].splitlines():
+                for tag in l.split(","):
+                    tag = tag.strip()
+                    if not tag:
+                        continue
+                    if tag not in self.data:
+                        self.data.append(tag)
 
     def _value(self):
         if self.data:
-            return ', '.join(self.data)
+            return ", ".join(self.data)
         else:
-            return ''
+            return ""
 
     @classmethod
     def _remove_duplicates(cls, seq):
@@ -36,4 +38,3 @@ class TagListField(Field):
 
 class TextListField(TagListField):
     widget = widgets.TextArea()
-
