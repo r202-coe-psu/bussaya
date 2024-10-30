7b58e115b5375454ab9868bd56210ff707378d95
teeranon124 <teeranon124@gmail.com>
2024-10-30 14:41:59 +0700

    fix: version

--- bussaya/cmd/web.py
+++ bussaya/cmd/web.py
@@ -5,4 +5,6 @@ def main():
     options = web.get_program_options()
     app = web.create_app()
 
+    print('debug', app.debug)
+
     app.run(debug=options.debug, host=options.host, port=int(options.port))
