d3901ed6cb7585fc362cf20374c58207b6fa7d2c
KIM2548TH <jitkon1979@gmail.com>
2024-12-02 20:17:56 +0700

    add new page view_info project

--- bussaya/web/views/admin/projects.py
+++ bussaya/web/views/admin/projects.py
@@ -52,6 +52,15 @@ def edit(project_id):
     return redirect(url_for("dashboard.index"))
 
 
+@module.route("/<project_id>/look", methods=["GET", "POST"])
+@acl.roles_required("admin")
+def view_info_project(project_id):
+    project = models.Project.objects.get(id=project_id)
+
+    return render_template("/projects/view-info-projects.html",
+                            project=project
+                            )
+
 @module.route("/<project_id>/delete")
 @acl.roles_required("admin")
 def delete(project_id):
